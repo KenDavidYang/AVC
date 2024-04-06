6913ab91f3c7ba83b03a2d99321a973c534da524
KenDavidYang <140172912+KenDavidYang@users.noreply.github.com>
2024-04-07 07:16:48 +0900

    updated icons

--- php/index.php
+++ php/index.php
@@ -1,5 +1,5 @@
 <?php
-
+ob_start();
 $connect = mysqli_connect (
     "db", #service name 
     "php_docker", # username
@@ -12,21 +12,32 @@ $lastName = $_POST['last-name'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 
+$options = [
+    'cost' => 12,
+];
+
+$hash = password_hash($password, PASSWORD_BCRYPT, $options);
+$query = "INSERT INTO account(firstName, lastName, email, password) VALUES('$firstName','$lastName','$email','$hash')";
+
+
 //database connection
-if($connect->connect_error) {
-    die("Connection Failed : ".$connect->connect_error);
-}
 if(isset($_POST['sign-up'])){
-    $query = mysqli_query($connect, "INSERT INTO registration(firstName, lastName, email, password) VALUES('$firstName','$lastName','$email','$password')");
+    $query = mysqli_query($connect, $query);
     if($query){
-        echo "<script>alert (data inserted successfuly)</script>";
+        echo "data inserted";
+        header("Location: http://localhost:8080");
+        ob_end_flush();
+        exit();
+       
     } else {
-        echo "<script>alert (data has not been inserted)</script>";
+        echo "data not inserted";
     }
 }
 
+
+
+
 #$stmt = $connect->prepare("INSERT INTO registration(firstName, lastName, email, password) values(:firstname,:lastName,:email,:password)");
 #$stmt->bindParam(":firstname", $firstName, PDO::PARAM_STR);
-
 
 ?>
