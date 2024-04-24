983bcefa4e992158ee60852bc58b79549a75f15f
hotdogcoder123 <106981712+hotdogcoder123@users.noreply.github.com>
2024-04-24 14:32:11 +0800

    Add files via upload

--- php/index.php
+++ php/index.php
@@ -7,31 +7,59 @@ $connect = mysqli_connect (
     "php_docker" #db table
 );
 
-$firstName = $_POST['first-name'];
-$lastName = $_POST['last-name'];
-$email = $_POST['email'];
-$password = $_POST['password'];
+//$firstName = $_POST['first-name'];
+//$lastName = $_POST['last-name'];
+//$email = $_POST['email'];
+//$password = $_POST['password'];
 
-$options = [
-    'cost' => 12,
-];
 
-$hash = password_hash($password, PASSWORD_BCRYPT, $options);
-$query = "INSERT INTO account(firstName, lastName, email, password) VALUES('$firstName','$lastName','$email','$hash')";
+
+
 
 
 //database connection
 if(isset($_POST['sign-up'])){
-    $query = mysqli_query($connect, $query);
-    if($query){
-        echo "data inserted";
+
+    $firstName = $_POST['first-name'];
+    $lastName = $_POST['last-name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $options = [
+        'cost' => 12,
+    ];
+    $hash = password_hash($password, PASSWORD_BCRYPT, $options);
+
+    $query_signup = "INSERT INTO account(firstName, lastName, email, password) VALUES('$firstName','$lastName','$email','$hash')";
+    $query_signup = mysqli_query($connect, $query_signup);
+
+    if($query_signup){
         header("Location: http://localhost:8080");
-        ob_end_flush();
-        exit();
-       
     } else {
         echo "data not inserted";
     }
+}
+
+function login($password, $hashedPassword) {
+    if(password_verify($password, $hashedPassword)) {
+        header("Location: http://localhost:8080");
+    } else {
+        $alert_message = "wrong password!";
+        header("Location: http://localhost:8080/html/login.html?alert=" . urlencode($alert_message));
+    }
+}
+
+if(isset($_POST['login'])){
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $query_login = "SELECT password FROM account WHERE email='$email'";
+
+
+    $query_login = mysqli_query($connect, $query_login);
+    $row = mysqli_fetch_row($query_login);
+    $cellValue = $row[0];
+    login($password, $cellValue);
 }
 
 
