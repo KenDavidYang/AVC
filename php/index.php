c4d5e20d24bc47dc01fb069aab7254b2a514ebd0
KenDavidYang <140172912+KenDavidYang@users.noreply.github.com>
2024-04-28 21:18:37 +0900

    za warudo

--- php/index.php
+++ php/index.php
@@ -7,15 +7,16 @@ $connect = mysqli_connect (
     "php_docker" #db table
 );
 
-//$firstName = $_POST['first-name'];
-//$lastName = $_POST['last-name'];
-//$email = $_POST['email'];
-//$password = $_POST['password'];
 
 
-
-
-
+function login($password, $hashedPassword) {
+    if(password_verify($password, $hashedPassword)) {
+        header("Location: http://localhost:8080");
+    } else {
+        $alert_message = "wrong password!";
+        header("Location: http://localhost:8080/html/login.html?alert=" . urlencode($alert_message));
+    }
+}
 
 //database connection
 // if value=sign-up button pressed go here
@@ -41,15 +42,6 @@ if(isset($_POST['sign-up'])){
     }
 }
 
-function login($password, $hashedPassword) {
-    if(password_verify($password, $hashedPassword)) {
-        header("Location: http://localhost:8080");
-    } else {
-        $alert_message = "wrong password!";
-        header("Location: http://localhost:8080/html/login.html?alert=" . urlencode($alert_message));
-    }
-}
-
 // if html value=login button pressed go here
 if(isset($_POST['login'])){
 
@@ -57,13 +49,36 @@ if(isset($_POST['login'])){
     $password = $_POST['password'];
     $query_login = "SELECT password FROM account WHERE email='$email'";
 
-
+    // takes a row with the email and fetches the database password
     $query_login = mysqli_query($connect, $query_login);
     $row = mysqli_fetch_row($query_login);
-    $cellValue = $row[0];
-    login($password, $cellValue);
+    $databasePassword = $row[0];
+    login($password, $databasePassword);
 }
 
+if(isset($_POST['order'])){
+    // putting input from order.html and puts it in a variable
+    $serviceType = $_POST['service-type'];
+    $paperSize = $_POST['paper-size'];
+    $paperType = $_POST['paper-type'];
+    $color = $_POST['color'];
+    $pageAmount = $_POST['number-of-pages'];
+    $quantity = $_POST['quantity'];
+    $finish = $_POST['finish'];
+    $coverType = $_POST['cover-type'];
+
+    // declares mysql query and runs it
+    $query_order_input = "INSERT INTO orders(service_type, paper_size, paper_type, color, number_of_pages, quantity, finish, cover_type) 
+    VALUES('$serviceType','$paperSize' ,'$paperType' ,'$color' ,'$pageAmount' ,'$quantity' ,'$finish' ,'$coverType')";
+    $query_order_input = mysqli_query($connect, $query_order_input);
+
+    // redirect to main page if no problem
+    if($query_order_input){
+        header("Location: http://localhost:8080");
+    } else {
+        echo "data not inserted";
+    }
+}
 
 
 
