ccc6e29c37449489cf07b9227b5e01f4b77646b5
C4tter <164760241+C4tter@users.noreply.github.com>
2024-06-02 19:12:42 +0800

    Add files via upload

--- php/db/php_docker.sql
+++ php/db/php_docker.sql
@@ -70,8 +70,8 @@ CREATE TABLE `orders` (
   `quantity` varchar(20) NOT NULL,
   `finish` varchar(20) NOT NULL,
   `coverType` varchar(20) NOT NULL,
-  `filename` varchar(200) NOT NULL,
-  `filesize` int(11) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `filesize` bigint NOT NULL,
   `filetype` varchar(100) NOT NULL,
   `checkOut_date` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
